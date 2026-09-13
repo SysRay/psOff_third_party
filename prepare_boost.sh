f44669dcd981faa4d04f7ffd05bdd949d026a6ba
igor725 <gvaldovigor@gmail.com>
2026-09-13 12:58:09 +0300

    Replace submodules with CMake Git cloning mechanism

---
+++ prepare_boost.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail;
+
+BOOST_VER=1.92.0
+LIBS=$(sed -n 's/.*BOOST_INCLUDE_LIBRARIES\s*"\([^"]*\)".*/\1/p' CMakeLists.txt | tr ';' ' ');
+if [ -d projects/boost ]; then
+  cd projects/boost;
+  git fetch --depth=1 origin;
+  git reset --hard boost-$BOOST_VER;
+  git gc --prune=now;
+else
+  git clone -b boost-$BOOST_VER --depth 1 https://github.com/boostorg/boost.git projects/boost;
+  cd projects/boost;
+fi
+git submodule update --init --depth 1 tools/boostdep;
+for lib in $LIBS; do
+  git submodule update --init --depth 1 "libs/$lib";
+  python tools/boostdep/depinst/depinst.py -g "--depth 1 --jobs 4" $lib;
+done
+cd libs/thread
+git apply ../../../../patches/boost_thread.patch
